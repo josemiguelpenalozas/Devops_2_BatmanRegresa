e91e629d75ec80039995e07091649ca1594e4d93
Jose Miguel <jo.penalozas@duocuc.cl>
2026-06-08 20:06:00 -0400

    se agrego nuevo formato archivos terraform y secret de kubernetes

---
+++ infra/terraform/vpc.tf
@@ -0,0 +1,82 @@
+############################
+# VPC
+############################
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "${var.project_name}-vpc"
+  }
+}
+
+############################
+# Subnets públicas
+############################
+
+resource "aws_subnet" "public_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "${var.aws_region}a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name                                                = "${var.project_name}-public-a"
+    "kubernetes.io/cluster/${var.project_name}-cluster" = "shared"
+    "kubernetes.io/role/elb"                            = "1"
+  }
+}
+
+resource "aws_subnet" "public_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "${var.aws_region}b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name                                                = "${var.project_name}-public-b"
+    "kubernetes.io/cluster/${var.project_name}-cluster" = "shared"
+    "kubernetes.io/role/elb"                            = "1"
+  }
+}
+
+############################
+# Internet Gateway
+############################
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.project_name}-igw"
+  }
+}
+
+############################
+# Route Tables
+############################
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-rt-public"
+  }
+}
+
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_b" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public.id
+}
